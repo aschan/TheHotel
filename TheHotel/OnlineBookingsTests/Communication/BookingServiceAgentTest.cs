b58eb3f90e11a6ade5512e45c15007126691dfbe
Edward Aschan <edward.aschan@acando.com>
2018-11-20 22:17:22 +0100

    Added initial unit test

---
+++ TheHotel/OnlineBookingsTests/Communication/BookingServiceAgentTest.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineBookings.Communication
+{
+    using System;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BookingServiceAgentTest
+    {
+        [Test]
+        [SetUICulture("en-US")]
+        public void ValidateConstructorInjectionOfIBookingSystemThrowsAnException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => new BookingServiceAgent(null));
+            Assert.That(exception.ParamName, Is.EqualTo("bookingSystem"));
+        }
+    }
+}
